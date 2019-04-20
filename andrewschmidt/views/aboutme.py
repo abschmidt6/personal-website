52232c765d5ea215d42cecedcab3d533e73e39a4
Andrew Schmidt <abschmidt6@gmail.com>
2019-04-20 13:54:32 -0400

    Initial commit

---
+++ andrewschmidt/views/aboutme.py
@@ -0,0 +1,16 @@
+"""
+Personal Website About Me view.
+
+URLs include:
+/
+"""
+import flask
+import arrow
+import andrewschmidt
+
+
+@andrewschmidt.app.route('/aboutme/', methods=['GET', 'POST'])
+def show_aboutme():
+    """Display / Route."""
+    context = dict()
+    return flask.render_template("aboutme.html", **context)
